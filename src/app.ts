656f047cb86aa11ff068958eb2713f252e2d9278
Eyuel Belete <eyuelbelete98@gmail.com>
2023-10-11 12:53:58 +0300

    update

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
 import express from 'express';
+import { errorHandler } from './middleware/errorHandler.middleware';
+import exampleRouter from './routes/example.routes';
   
 const app = express();
+app.use(express.json());
+
 const port = 9000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+// Use the route modules
+app.use('/api', exampleRouter);
+// Add the error handling middleware
+app.use(errorHandler);
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
---
+++ src/controller/example.controller.ts
@@ -0,0 +1,10 @@
+
+
+import { Request, Response } from 'express';
+
+export const exampleGetController = (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Hello from get controller!' });
+};
+export const examplePostController = (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Hello from post controller!' });
+};
