ff7e28a180b8721d19b1b7c0576a427b504f5e3a
Eyuel Belete <eyuelbelete98@gmail.com>
2023-10-20 02:59:06 +0300

    stand alone tus server side api

--- src/config/tus.config.ts
+++ src/config/tus.config.ts
@@ -1,25 +1,12 @@
 
 import * as tus from "tus-node-server";
-import { bucketName, minioClient } from "./minio.config";
-
-//minio store configuration data to be used as S3Store
-const store = {
-  partSize: 8 * 1024 * 1024, // Each uploaded part will have ~8MB,
-  bucket: bucketName,
-  accessKeyId: process.env.MINIO_ROOT_USER || "",
-  secretAccessKey: process.env.MINIO_ROOT_PASSWORD || "",
-  endpoint: "http://localhost:9000",
-  s3ForcePathStyle: true,
-};
-
 
 const serverOptions = {
   path: "/upload/tus/test",
 };
 const tusServer = new tus.Server(serverOptions);
 
-tusServer.datastore = new tus.S3Store(store);
-// tusServer.datastore = new tus.FileStore({
-//   directory: 'C:/Users/eyuel/Documents/Tus-minio/api/files'
-// });
+tusServer.datastore = new tus.FileStore({
+  directory: 'C:/Users/eyuel/Desktop/typescript/files'
+});
 export { tusServer };
--- src/controller/example.controller.ts
+++ src/controller/example.controller.ts
@@ -5,19 +5,3 @@ export const exampleGetController = (req: Request, res: Response) => {
 export const examplePostController = (req: Request, res: Response) => {
   res.status(200).json({ message: "Hello from post controller!" });
 };
-
-export const exampleMulterUploadController = (req: Request, res: Response) => {
-  res.status(200).json({ message: "File uploaded!" });
-};
-export const exampleMinioWithMulterUploadController = (req: Request, res: Response) => {
-  if (res.locals.etag) {
-    res.status(200).json({
-      message: "File uploaded successfully.",
-      filename: req.file && req.file.filename ? req.file.filename : '', // Include the uploaded file name
-      etag: res.locals.etag, // Include the etag value
-    });
-  } else {
-    res.status(400).json({ message: "File upload failed." });
-  }
-};
-
