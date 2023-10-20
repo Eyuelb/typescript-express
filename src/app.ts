b19abff88eede54c9c86ddb24212fa3fb7de5d28
Eyuel Belete <eyuelbelete98@gmail.com>
2023-10-20 03:18:51 +0300

    change port

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import exampleRouter from './routes/example.routes';
 const app = express();
 app.use(express.json());
 
-const port = 9000;
+const port = 4000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
